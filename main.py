e695b0aa8f60cf5d343af98818f04f30d1f4ae62
Regina1965 <149797487+Regina1965@users.noreply.github.com>
2024-02-13 16:51:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ for i in range(6000):
 print("E")
 print('A')
 print('B')
-for i in range(20):
+for i in range(200):
     print('C')
-for i in range(100):
+for i in range(10):
     print('D')
 print('E')
